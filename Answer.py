fdf62472cad38f4037530808531a9d101f492e5a
Aryan <aryansahal19@gmail.com>
2023-06-07 16:27:34 +0530

    final upload

--- Answer.py
+++ Answer.py
@@ -1,4 +1,3 @@
-from ctypes import sizeof
 import json
 from datetime import datetime
 from collections import defaultdict
@@ -9,10 +8,8 @@ def calculate_balance_sheet(input_data):
     revenue_data = input_data['revenueData']
     month_list = []
 
-    # Create a dictionary to store the balances for each month
     balance_sheet = defaultdict(int)
 
-    # Calculate the balance for each revenue entry
     for entry in revenue_data:
         timestamp = entry.get('startDate')
         amount = entry.get('amount', 0)
@@ -21,7 +18,6 @@ def calculate_balance_sheet(input_data):
 
         balance_sheet[month] += amount
 
-    # Calculate the balance for each expense entry
     for entry in expense_data:
         timestamp = entry.get('startDate')
         amount = entry.get('amount', 0)
@@ -31,15 +27,12 @@ def calculate_balance_sheet(input_data):
         balance_sheet[month] -= amount
     month_list = list(dict.fromkeys(month_list))
 
-    # Print the balance sheet
     for month in range(1, max(month_list)+1):
         balance = balance_sheet[month]
         print(f"Month: {month:02d}, Balance: {balance}")
 
 
-# Read the input data from a JSON file
 with open('1-input.json') as file:
     input_data = json.load(file)
 
-# Calculate and print the balance sheet
 calculate_balance_sheet(input_data)
