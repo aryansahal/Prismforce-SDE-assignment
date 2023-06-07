5b7adcab1e393e0c3d434f1bb8230e1ce9907267
Aryan <aryansahal19@gmail.com>
2023-06-07 15:48:27 +0530

    final

--- Answer.py
+++ Answer.py
@@ -1,38 +1,45 @@
+from ctypes import sizeof
 import json
+from datetime import datetime
+from collections import defaultdict
 
 
 def calculate_balance_sheet(input_data):
     expense_data = input_data['expenseData']
     revenue_data = input_data['revenueData']
-    balance_sheet = {}
+    month_list = []
 
+    # Create a dictionary to store the balances for each month
+    balance_sheet = defaultdict(int)
+
+    # Calculate the balance for each revenue entry
     for entry in revenue_data:
         timestamp = entry.get('startDate')
         amount = entry.get('amount', 0)
-        month = timestamp.split('-')[1]
+        month = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%fZ").month
+        month_list.append(month)
 
-        if month not in balance_sheet:
-            balance_sheet[month] = amount
-        else:
-            balance_sheet[month] += amount
+        balance_sheet[month] += amount
 
+    # Calculate the balance for each expense entry
     for entry in expense_data:
         timestamp = entry.get('startDate')
         amount = entry.get('amount', 0)
-        month = timestamp.split('-')[1]
+        month = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%fZ").month
+        month_list.append(month)
 
-        if month not in balance_sheet:
-            balance_sheet[month] = -amount
-        else:
-            balance_sheet[month] -= amount
+        balance_sheet[month] -= amount
+    month_list = list(dict.fromkeys(month_list))
 
-    sorted_balance_sheet = sorted(balance_sheet.items(), key=lambda x: x[0])
-
-    for month, balance in sorted_balance_sheet:
-        print(f"Month: {month}, Balance: {balance}")
+    # Print the balance sheet
+    for month in range(1, max(month_list)+1):
+        balance = balance_sheet[month]
+        print(f"Month: {month:02d}, Balance: {balance}")
 
 
+# Read the input data from a JSON file
 with open('1-input.json') as file:
     input_data = json.load(file)
 
+# Calculate and print the balance sheet
 calculate_balance_sheet(input_data)
